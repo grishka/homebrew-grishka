ee351a5ed76163119051db9b56880b166b937b39
Filippo Fortino <filippofortino@gmail.com>
2025-08-08 10:47:46 +0200

    fix: release url

--- Casks/neardrop.rb
+++ Casks/neardrop.rb
@@ -2,7 +2,7 @@ cask "neardrop" do
   version "2.2.0"
   sha256 "ee6fed09714487789f605198c0415bfd097b61efa527dd21560e10af5c56a6a5"
 
-  url "https://github.com/grishka/NearDrop/releases/download/v#{version}/NearDrop.app.zip"
+  url "https://github.com/grishka/NearDrop/releases/download/v#{version}/NearDrop.zip"
   name "NearDrop"
   desc "Unofficial Google Nearby Share app"
   homepage "https://github.com/grishka/NearDrop/"
